52dc0b8d1a21f87bd3d9326a5e24319aed329e48
Davi <69681778+pessoa736@users.noreply.github.com>
2025-10-12 20:21:58 -0300

    fix: disable additional TypeScript ESLint rule for empty object type

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,7 @@ const eslintConfig = [
     ],
     rules:{
       "@typescript-eslint/no-empty-object": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-empty-interface": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-function": "off",
